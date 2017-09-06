c40f76fa4de561649b2128c3a2b397b3237dbe0c
Yannick <axmann.yannick@hotmail.com>
2017-09-06 18:23:20 +0200

    Terminal Update

--- terminal.php
+++ terminal.php
@@ -2,21 +2,33 @@
 <html>
 <head>
   <title>Terminal</title>
-  <link rel="stylesheet" href="/css/terminal.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <script type="text/javascript">
-      
-  </script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/terminal.css">
+
 </head>
 <body>
 
   <div id="terminalbody">
     <div id="termtitle">
-      <h1>Terminal</h1>
-      <hr/>
+     <div class=" termbox2 col-md-12">
+        <div class="col-md-4">
+          <h1>Terminal</h1>
+        </div>
+
+        <div id="links" class="col-md-8">
+        <a href="/commands.php">Help</a>
+        <span> | </span>
+        <a href="/index.html">Zurück</a>
+        </div>
+      </div>
     </div>
-    <div id="terminal">
-        <p id="termcode">
+    <div id="terminal" class="termbox1 col-md-12">
+        <p>
         <?php
           
           echo 'Note: Change Directory not possible!<br/><br/>';
@@ -30,6 +42,7 @@
             echo '<pre>';
             echo "Executed: /$com".'<br/><br/>';
             echo system($com);
+            echo '<br/><br/>';
             echo '</pre>';
             }
         ?>  
@@ -37,12 +50,12 @@
     </div>
 
     <div id="input">
-      <div id="inbox">
+      <div>
         <form action="terminal.php" method="post">
           <input type="text" name="command">
           <br/><br/>
           <input id="enter" type="Submit" value="Run">
-        <form>
+        </form>
       </div>
     </div>
   </div>
